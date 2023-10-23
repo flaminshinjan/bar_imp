21e608ba103346e2663b13e5cf7de6603e60cf1b
flaminshinjan <srikpatra.2008@gmail.com>
2023-10-23 15:30:26 +0530

    Initial Commit

---
+++ bar_imp/bar_impApp.swift
@@ -0,0 +1,17 @@
+//
+//  bar_impApp.swift
+//  bar_imp
+//
+//  Created by Shinjan Patra on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct bar_impApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
